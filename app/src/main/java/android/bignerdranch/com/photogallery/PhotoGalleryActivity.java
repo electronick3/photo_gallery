1c7fbc18d11879c2cc8ea687d2dc24efc98f6241
Chagay Nickolay <chagay.ni@gmail.com>
2019-09-12 11:46:55 +0300

    Init commit

---
+++ app/src/main/java/android/bignerdranch/com/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,19 @@
+package android.bignerdranch.com.photogallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return super.getLayoutResId();
+    }
+}
